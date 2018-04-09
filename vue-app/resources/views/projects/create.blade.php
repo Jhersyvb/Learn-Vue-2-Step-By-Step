542475f3675ed2570e61f1053183e2e102f34e9e
Jhersy Valer Bejarano <Jhersyvb@gmail.com>
2018-04-09 12:11:03 -0500

    Episode 20. Object-Oriented Forms: Part 2

--- vue-app/resources/views/projects/create.blade.php
+++ vue-app/resources/views/projects/create.blade.php
@@ -13,25 +13,25 @@
     <div id="app" class="container">
         @include('projects.list')
 
-        <form method="POST" action="/projects" @submit.prevent="onSubmit" @keydown="errors.clear($event.target.name)">
+        <form method="POST" action="/projects" @submit.prevent="onSubmit" @keydown="form.errors.clear($event.target.name)">
             <div class="field">
                 <label for="name" class="label">Project Name:</label>
                 <div class="control">
-                    <input type="text" id="name" name="name" class="input" v-model="name">
+                    <input type="text" id="name" name="name" class="input" v-model="form.name">
                 </div>
-                <span class="help is-danger" v-if="errors.has('name')" v-text="errors.get('name')"></span>
+                <span class="help is-danger" v-if="form.errors.has('name')" v-text="form.errors.get('name')"></span>
             </div>
 
             <div class="field">
                 <label for="description" class="label">Project Description:</label>
                 <div class="control">
-                    <input type="text" id="description" name="description" class="input" v-model="description">
+                    <input type="text" id="description" name="description" class="input" v-model="form.description">
                 </div>
-                <span class="help is-danger" v-if="errors.has('description')" v-text="errors.get('description')"></span>
+                <span class="help is-danger" v-if="form.errors.has('description')" v-text="form.errors.get('description')"></span>
             </div>
 
             <div class="control">
-                <button class="button is-primary" :disabled="errors.any()">Create</button>
+                <button class="button is-primary" :disabled="form.errors.any()">Create</button>
             </div>
         </form>
     </div>
