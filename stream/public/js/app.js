6d8518f3bd7ee7a29e3088a45b4d3d781407b5c5
Jhersy Valer Bejarano <Jhersyvb@gmail.com>
2018-04-10 12:33:10 -0500

    Episode 28. Vue Filters

--- stream/public/js/app.js
+++ stream/public/js/app.js
@@ -15984,19 +15984,23 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
             statuses: []
         };
     },
+
+
+    filters: {
+        ago: function ago(date) {
+            return __WEBPACK_IMPORTED_MODULE_0_moment___default()(date).fromNow();
+        },
+        capitalize: function capitalize(value) {
+            return value.toUpperCase();
+        }
+    },
+
     created: function created() {
         var _this = this;
 
         __WEBPACK_IMPORTED_MODULE_1__models_Status__["a" /* default */].all(function (statuses) {
             return _this.statuses = statuses;
         });
-    },
-
-
-    methods: {
-        postedOn: function postedOn(status) {
-            return __WEBPACK_IMPORTED_MODULE_0_moment___default()(status.created_at).fromNow();
-        }
     }
 });
 
@@ -16027,7 +16031,9 @@ var render = function() {
               _c("p", [
                 _vm._v(
                   "\n                        " +
-                    _vm._s(_vm.postedOn(status)) +
+                    _vm._s(
+                      _vm._f("capitalize")(_vm._f("ago")(status.created_at))
+                    ) +
                     "\n                    "
                 )
               ])
