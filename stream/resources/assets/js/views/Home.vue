52e1785901416c673ffd24330e186855c8f9e09a
Jhersy Valer Bejarano <Jhersyvb@gmail.com>
2018-04-10 10:39:15 -0500

    Episode 26. Vue SPA Essentials: Routing

---
+++ stream/resources/assets/js/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="panel">
+                    <div class="panel-heading">Home Page</div>
+
+                    <div class="panel-block">
+                        I'm an example component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
--- vue-app/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/projects/create', 'ProjectsController@create');
-Route::post('projects', 'ProjectsController@store');
