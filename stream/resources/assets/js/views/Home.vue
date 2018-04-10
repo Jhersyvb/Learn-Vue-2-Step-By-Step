a9c8aa706093a992afa08bb0c3644a43dd1ded74
Jhersy Valer Bejarano <Jhersyvb@gmail.com>
2018-04-10 12:16:54 -0500

    Episode 27. SPAs and the Backend

--- stream/resources/assets/js/views/Home.vue
+++ stream/resources/assets/js/views/Home.vue
@@ -1,12 +1,19 @@
 <template>
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="panel">
-                    <div class="panel-heading">Home Page</div>
+        <div class="columns">
+            <div class="column">
+                <div class="message" v-for="status in statuses">
+                    <div class="message-header">
+                        <p>
+                            {{ status.user.name }} said...
+                        </p>
 
-                    <div class="panel-block">
-                        I'm an example component.
+                        <p>
+                            {{ postedOn(status) }}
+                        </p>
+                    </div>
+
+                    <div class="panel-block" v-text="status.body">
                     </div>
                 </div>
             </div>
@@ -15,9 +22,24 @@
 </template>
 
 <script>
+    import moment from 'moment';
+    import Status from '../models/Status';
+
     export default {
-        mounted() {
-            console.log('Component mounted.')
+        data() {
+            return {
+                statuses: []
+            }
+        },
+
+        created() {
+            Status.all(statuses => this.statuses = statuses);
+        },
+
+        methods: {
+            postedOn(status) {
+                return moment(status.created_at).fromNow();
+            }
         }
     }
 </script>
